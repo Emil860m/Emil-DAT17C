d574c82da3a71bfb2afb5aadba6a92add4ac92f3
Emil860m <fenger.emil@gmail.com>
2018-10-10 21:47:24 +0200

    Change

--- src/MandatoryAssignment1/TCPServer.java
+++ src/MandatoryAssignment1/TCPServer.java
@@ -6,91 +6,99 @@ import java.net.Socket;
 import java.util.ArrayList;
 
 public class TCPServer {
-    public static void main(String[] args) throws IOException {
-        System.out.println("Server");
-        final int PORT_IN = 5656;
-        final int PORT_OUT = 5657;
-        ArrayList<Client> clients = new ArrayList();
+    final static ArrayList<Client> clients = new ArrayList<>();
+    public static void main(String [] args){
+        final int PORT = 5656;
 
 
-        System.out.println("starting TCP Server main program");
+        try{
+            ServerSocket server = new ServerSocket(PORT);
+            while(true){
 
-        Thread newClients = new Thread(() -> {
-            while (true) {
-                try {
-                    int clientCount = 0;
-                    ServerSocket server = new ServerSocket(PORT_IN);
-                    System.out.println("waiting for a connection");
-                    Socket socket = server.accept();
+                final Socket socket = server.accept();
+                System.out.println("Client connected");
 
-                    Thread tempClient = new Thread(() -> {
-                        try {
-                            Socket tempSocket = socket;
-                            BufferedReader inFromClient = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                            DataOutputStream outToClient = new DataOutputStream(socket.getOutputStream());
-                            String clientInput = null;
-                            clientInput = inFromClient.readLine();
-                            String[] tempArray = clientInput.split(",");
-                            //if (tempArray[0].equalsIgnoreCase("JOIN")) {
-                                //Client.incrementClientCount();
-                                outToClient.writeBytes("J_OK");
-                                //Client client = new Client(tempArray[1], tempSocket, inFromClient, outToClient, Client.getClientCount());
-                                //clients.add(client);
+                try{
+                    InputStream input = socket.getInputStream();
+                    byte[] dataIn = new byte[1024];
+                    input.read(dataIn);
+                    String message = new String(dataIn);
+                    message = message.trim();
 
-                                Thread thread = new Thread(()->{
-                                    try {
-                                        String input = inFromClient.readLine();
-                                        String[] temp = input.split(",");
-                                        if(temp[0].equalsIgnoreCase("IMAV")){
+                    if(message.contains("JOIN")){
+                        int indexOfComma = message.lastIndexOf(",");
+                        String userName = message.substring(5, indexOfComma);
+                       // if(checkUserName(temp2[1])){
+                            Client client = new Client();
+                            client.setUserName(userName);
+                            client.setSocket(socket);
+                            client.setOutToClient(socket.getOutputStream());
+                            client.setInFromClient(socket.getInputStream());
+                            client.setIP(socket.getInetAddress().getHostAddress());
+                            client.setPort(socket.getPort());
+                            client.setConnected(true);
 
+                            clients.add(client);
+                            client.getOutToClient().write("J_OK \n".getBytes());
+                            sendMessageToAll("List of clients: " + clients.toString());
+                            System.out.println("Client added");
+
+                            ArrayList<Thread> receiveThreads = new ArrayList<>();
+                            Thread receive = new Thread(()->{
+                                while(true){
+                                    try{
+                                        InputStream inputStream = client.getInFromClient();
+                                        byte[] inFromClient = new byte[1024];
+                                        inputStream.read(inFromClient);
+                                        String msg = new String(inFromClient);
+                                        msg.trim();
+                                        //System.out.println(msg);
+                                        //System.out.println(msg);
+                                        if(msg.contains("IMAV")){
+                                            client.setSecondsSinceIMAV(0);
+                                        }else if(msg.contains("QUIT")){
+                                            System.out.println("Client disconnected!");
+                                            client.getOutToClient().write("QUIT".getBytes());
+                                            client.setConnected(false);
+                                            sendMessageToAll(client.getUserName()+ " disconnected!");
+                                            break;
+                                        }else if(msg.contains("DATA")){
+                                            msg = msg.replaceAll("DATA ", "");
+                                            String msgToAll = client.getUserName() + ": " + msg;
+
+                                            System.out.println(msgToAll);
+                                            sendMessageToAll(msgToAll);
+                                        }else{
+                                            client.getOutToClient().write("ERROR: Try one of these commands: DATA <<Free text>> or QUIT".getBytes());
                                         }
-                                    } catch (IOException e) {
-                                        e.printStackTrace();
+
+
+
+                                    }catch (Exception e){
+
                                     }
-                                });
+                                }
+                            });
+                            receiveThreads.add(receive);
+                            for (Thread t:receiveThreads) {
+                                t.start();
+                            }
+                        //}
 
-                            //}
-                        } catch (IOException e) {
 
-                        }
-                    });
 
-                    tempClient.start();
-                } catch (Exception e) {
+
+                    }
+
+
+                }catch (Exception e){
 
                 }
+
+
             }
-        });
+        }catch (Exception e){
 
-        newClients.start();
-        try {
-            newClients.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        try {
-//            String clientIp = socket.getInetAddress().getHostAddress();
-//            System.out.println("IP: " + clientIp);
-//            System.out.println("PORT: " + socket.getPort());
-
-            boolean test = true;
-            do {
-                String clientInput = null;
-
-                //clientInput = inFromClient.readLine();
-//                if(){//Join cmd
-//                    outToClient.writeBytes("J_OK \n");
-//                } else if(){
-//
-//                }else if(){
-//
-//                }
-                System.out.println("FROM CLIENT: " + clientInput);
-                //outToClient.writeBytes(clientInput + '\n');
-                if(clientInput.equalsIgnoreCase("quit"))test = false;
-            }while(test);
-        } catch (Exception e) {
-            //e.printStackTrace();
         }
 
 
@@ -98,4 +106,28 @@ public class TCPServer {
 
     }
 
+    public static boolean checkUserName(String userName){
+        for (Client c: clients) {
+            if(c.getUserName().equalsIgnoreCase(userName)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void send(OutputStream output, String message){
+        try{
+            byte[] dataToSend = message.getBytes();
+            output.write(dataToSend);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+    public static void sendMessageToAll(String message){
+        for (Client c:clients) {
+            if(c.isConnected()){
+                send(c.getOutToClient(), message);
+            }
+        }
+    }
 }
