5e2a88d8733c8c725ab909e3bc7df0cc5d3e1049
Emil860m <fenger.emil@gmail.com>
2018-10-22 11:38:21 +0200

    daiojsm

--- src/MVC/view/PersonVeiw.java
+++ src/MVC/view/PersonVeiw.java
@@ -27,14 +27,6 @@ public class PersonVeiw {
         String type = P.getClass().toString().substring(16,P.getClass().toString().length());
         result += "<h1>Exam("+type+"): " + P.doExam() + "</h1>\n";
         result += "<h1>Type: " + type + "</h1>\n";
-//        if(P instanceof Teacher){
-//            result += "<h1>Exam(Teacher): " + P.doExam() + "</h1>";
-//            result += "<h1>Type: Teacher </h1>";
-//        }else if(P instanceof Student){
-//            result += "<h1>Exam(Student): " + P.doExam() + "</h1>";
-//            result += "<h1>Type: Student </h1>";
-//        }
-
         result += "</body>";
         return result;
     }
